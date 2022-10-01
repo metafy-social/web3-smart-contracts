4d096e45771e03ca483fe10fd17ae0f68e963cc0
Aditya Bhattad <adityabhattad18@gmail.com>
2022-10-01 11:30:58 +0530

    Added Commnents on functions

--- contracts/NFT/erc1155.sol
+++ contracts/NFT/erc1155.sol
@@ -10,18 +10,31 @@ import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Supply.sol";
 contract AdvaitaNFT is ERC1155, Ownable, Pausable, ERC1155Burnable, ERC1155Supply {
     constructor() ERC1155("ipfs://") {}
 
+
+    /**
+     * @dev Set a new URI for metadata, only owner can call this function
+    */
     function setURI(string memory newuri) public onlyOwner {
         _setURI(newuri);
     }
 
+    /** 
+     * @dev Pause the contract, only owner can call this function
+     */
     function pause() public onlyOwner {
         _pause();
     }
 
+    /** 
+     * @dev Unpause the contract, only owner can call this function
+     */
     function unpause() public onlyOwner {
         _unpause();
     }
 
+    /**
+     * @dev Mint new NFTs, only owner can call this function
+     */
     function mint(
         address account,
         uint256 id,
@@ -31,6 +44,9 @@ contract AdvaitaNFT is ERC1155, Ownable, Pausable, ERC1155Burnable, ERC1155Suppl
         _mint(account, id, amount, data);
     }
 
+    /**
+     * @dev  Mint copies of multiple tokenIDs, only owner can call this function
+     */
     function mintBatch(
         address to,
         uint256[] memory ids,
@@ -40,6 +56,9 @@ contract AdvaitaNFT is ERC1155, Ownable, Pausable, ERC1155Burnable, ERC1155Suppl
         _mintBatch(to, ids, amounts, data);
     }
 
+    /**
+     * @dev Keeps track of total supply of tokens.
+     */
     function _beforeTokenTransfer(
         address operator,
         address from,
