7c6a72a570af46f35378195ae2ebf7b3c6e916ba
Neluka <nelukarulzz@gmail.com>
2022-10-08 09:25:21 +0530

    memo defi  files added

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,6 @@
-require("@nomicfoundation/hardhat-toolbox");
+// require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-waffle");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
