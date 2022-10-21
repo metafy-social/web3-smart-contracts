14e8b589399878a18a3582d08d1df884b67dfdfe
antoprince001 <antoprince001@gmail.com>
2022-10-21 18:01:44 +0530

    Add blockchain based DNS smart contract #132

---
+++ contracts/blockDNS/Description.md
@@ -0,0 +1,7 @@
+# blockDNS
+
+blockDNS is highly basic implementation of the Domain Name System server built using ethereum. The smart contract is developed in solidity.
+DNS translates domain names into corresponding ip addresses so the browser can load the internet resources.
+
+
+https://antoprince001.medium.com/developing-a-blockchain-based-domain-name-system-dns-with-ethereum-c631d8c67bbd
---
+++ contracts/blockDNS/dns.sol
@@ -0,0 +1,74 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.21 <0.7.0;
+
+contract Dns{
+    
+    
+    struct Domain{
+        string domainName;
+        string A_Record;
+        string CNAME;
+        string TXT;
+        string NS_Record;
+        string SOA_Record;
+        bool exists;
+    }
+    
+    mapping (string => Domain) public nameserver;
+    
+    
+    
+    function addDomain(string memory _domainName , string memory _A_Record , string memory _CNAME, string memory _TXT ,string memory _NS_Record, string memory _SOA_Record) public returns(bool) {
+        require(nameserver[_domainName].exists == false,"IP mapping already exists");
+        
+        Domain memory new_domain = Domain(_domainName,_A_Record,_CNAME,_TXT,_NS_Record,_SOA_Record,true);
+        nameserver[_domainName] = new_domain;
+        
+        return true;
+    }
+   
+    
+    function dnsLookup(string memory _domainName) public view returns (string memory,string memory,string memory,string memory, string memory){
+        require(nameserver[_domainName].exists == true,"DNS not configured yet for the domain");
+        
+        return ( nameserver[_domainName].A_Record, nameserver[_domainName].CNAME, nameserver[_domainName].TXT,nameserver[_domainName].NS_Record,nameserver[_domainName].SOA_Record);
+    }
+    
+    function modifyARecord(string memory _domainName, string memory _A_Record) public returns (bool){
+        require(nameserver[_domainName].exists == true,"DNS not configured yet for the domain");
+        
+        nameserver[_domainName].A_Record = _A_Record;
+        return true;
+        
+    }
+    
+    function modifyCNAMERecord(string memory _domainName, string memory _CNAME) public returns (bool){
+        require(nameserver[_domainName].exists == true,"DNS not configured yet for the domain");
+        
+        nameserver[_domainName].CNAME = _CNAME;
+        return true;
+        
+    }
+    
+    function modifyTXTRecord(string memory _domainName,string memory _TXT) public returns (bool){
+        require(nameserver[_domainName].exists == true,"DNS not configured yet for the domain");
+        
+        nameserver[_domainName].TXT = _TXT;
+        return true;
+    }
+    
+    function modifyNSRecord(string memory _domainName,string memory _NS_Record) public returns (bool){
+        require(nameserver[_domainName].exists == true,"DNS not configured yet for the domain");
+        
+        nameserver[_domainName].NS_Record = _NS_Record;
+        return true;
+    }
+    
+    function modifySOARecord(string memory _domainName,string memory _SOA_Record) public returns (bool){
+        require(nameserver[_domainName].exists == true,"DNS not configured yet for the domain");
+        
+        nameserver[_domainName].SOA_Record = _SOA_Record;
+        return true;
+    }
+    
+}
