81a10dfd5b56fd745ebdf838262cde49bcb7ceb2
upendra <upendra@upendras-MacBook-Air.local>
2022-12-15 23:04:52 +0530

    Added modifier to remove duplication

--- contracts/Banking/BankingSystem.sol
+++ contracts/Banking/BankingSystem.sol
@@ -2,24 +2,26 @@
 pragma solidity ^0.8.0;
 
 contract BankingSystem {
-
     mapping(address => uint256) public balanceOf;   // balanceOf, indexed by addresses
+
+    // This is the modifier that will check if the caller has sufficient funds
+    modifier hasSufficientFunds(uint _amount) {
+        require (balanceOf[msg.sender] >= _amount, "Insufficient Funds");
+        _;
+    }
 
     function deposit() public payable {
         balanceOf[msg.sender] += msg.value;
     }
 
-    function withdraw(uint _amount) public {
-        require (balanceOf[msg.sender] >= _amount, "Insufficent Funds");
+    function withdraw(uint _amount) public hasSufficientFunds(_amount) {
         balanceOf[msg.sender] -= _amount;
         (bool sent,) = msg.sender.call{value: _amount}("Sent");
         require(sent, "Failed to Complete");
     }
 
     // transferAmt function transfers ether from one account to another
-    function transferAmt(address payable _address, uint _amount) public {
-        require (balanceOf[msg.sender] >= _amount, "Insufficent Funds");
+    function transferAmt(address payable _address, uint _amount) public hasSufficientFunds(_amount) {
         _address.transfer(_amount);
     }
-
 }
